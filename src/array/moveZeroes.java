2972463442b60563613c1441a16832e04dd1dde9
joeliang <>
2026-08-10 16:54:12 +0800

    init: 初始化 Java 项目，添加 moveZeroes 题解

---
+++ src/array/moveZeroes.java
@@ -0,0 +1,26 @@
+package array;
+
+public class moveZeroes {
+    
+    public void moveZeroes(int[] nums) {
+        
+        int n = nums.length, left = 0, right = 0;
+        
+        while (right < n) {
+            if (nums[right] != 0) {
+                swap(nums, left, right);
+                left++;
+            }
+
+            right++;
+        }
+        
+    }
+
+    void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+    
+}
